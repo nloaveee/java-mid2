da991656a73fe0723243f41e838e6bfc9bd44144
hyeonheee <nloavee@gmail.com>
2024-10-02 15:27:44 +0900

    set

--- src/collection/set/MyHashSetV0Main.java
+++ src/collection/set/MyHashSetV0Main.java
@@ -9,7 +9,13 @@ public class MyHashSetV0Main {
         set.add(3);
         set.add(4);
         set.add(5);
-
         System.out.println(set);
+
+        boolean result = set.add(4);
+        System.out.println("중복 데이터 저장 결과 = " + result);
+        System.out.println(set);
+
+        System.out.println("set.contains(3) = "+set.contains(3)); //O(n)
+        System.out.println("set.contains(99) = "+set.contains(99)); //O(n)
     }
 }
